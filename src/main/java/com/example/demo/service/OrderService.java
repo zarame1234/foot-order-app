270891f2ae7709a33e3767fd408e6fd7f3d809a4
zarame1234 <zarame1234@users.noreply.github.com>
2026-07-04 13:39:56 +0900

    注文操作追加

--- src/main/java/com/example/demo/service/OrderService.java
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -1,20 +1,28 @@
 package com.example.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Order;
+import com.example.demo.repository.OrderRepository;
 
 @Service
 public class OrderService {
+  public final OrderRepository orderRepository;
+  public OrderService(OrderRepository orderRepository) {
+    this.orderRepository = orderRepository;
+  }
+
+  public List<Order> getOrders() {
+    return orderRepository.findAll();  
+  }
+}
   
+  /*
   public List<Order> getOrders() {
     List<Order> orders = new ArrayList<>();
       orders.add(new Order(1L, 2L));
       orders.add(new Order(2L, 1L));
 
-        return orders;
+      return orders;
   }
-}
+  */
