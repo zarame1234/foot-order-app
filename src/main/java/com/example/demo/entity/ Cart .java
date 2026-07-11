767ac17262ef364ec673ac40f63fbe0895aef374
zarame1234 <zarame1234@users.noreply.github.com>
2026-07-11 09:18:20 +0900

    カートEntityシート作成

--- src/main/java/com/example/demo/entity/ Cart .java
+++ src/main/java/com/example/demo/entity/ Cart .java
@@ -1,5 +1,39 @@
 package com.example.demo.entity;
 
-public class  Cart  {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cart {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Long productId;
+  private Integer quantity;
+
+  public Long getId() {
+    return id;
+  }
+
+  public Long getProductId() {
+    return productId;
+  }
+
+  public Integer getQuantity() {
+    return  quantity;
+  }
   
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setProductId(Long productId) {
+    this.productId = productId;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
 }
