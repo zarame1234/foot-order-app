61763b17d1ea34470ad831e542b2239ba149dbad
zarame1234 <zarame1234@users.noreply.github.com>
2026-07-11 09:25:34 +0900

    ファイル名修正

--- src/main/java/com/example/demo/repository/CartRepository.java
+++ src/main/java/com/example/demo/repository/CartRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repository;
 
-public class CartRepository extens JpaRepository<Cart, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.Cart;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
   
 }
