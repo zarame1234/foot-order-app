6174780ab998929d28beb648a8272a638aa36f5a
zarame1234 <zarame1234@users.noreply.github.com>
2026-07-16 19:39:11 +0900

    OrderFormにsetter追加

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -4,10 +4,34 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import com.example.demo.service.OrderService;
+import com.example.demo.service.CartService;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.example.demo.entity.Order;
+@Controller
+public class OrderController {
+  private final CartService cartService;
+  public OrderController(CartService cartService) {
+    this.cartService = cartService;
+  }
+
+  @GetMapping("/orders")
+  public String showOrder(Model model) {
+    model.addAttribute("cartItems",
+    cartService.getCartList());
+    return "order";
+  }
+  @PostMapping("/orders")
+  public String order() {
+    orderService.createOrder();
+    return "redirect:/orders/complete";
+  }
+}
+  
 
 
 /*注文履歴
