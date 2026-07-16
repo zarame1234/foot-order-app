7cca694cfb630955efec45042e8541f3591d72e2
zarame1234 <zarame1234@users.noreply.github.com>
2026-07-16 20:09:32 +0900

    注文処理を追加

--- src/main/java/com/example/demo/service/OrderService.java
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -1,19 +1,42 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Cart;
 import com.example.demo.entity.Order;
+import com.example.demo.repository.CartRepository;
 import com.example.demo.repository.OrderRepository;
 
 @Service
 public class OrderService {
   public final OrderRepository orderRepository;
-  public OrderService(OrderRepository orderRepository) {
+  private final CartRepository cartRepository;
+  public OrderService(OrderRepository orderRepository, CartRepository cartRepository) {
     this.orderRepository = orderRepository;
+    this.cartRepository = cartRepository;
   }
 
   public List<Order> getOrders() {
     return orderRepository.findAll();  
+  }
+  
+  /**
+   * 注文を確定する
+   */
+  public void createOrder() {
+    List<Cart> cartList = cartRepository.findAll();
+    for (Cart cart : cartList) {
+      Order order = new Order();
+
+      order.setProduct(cart.getProduct());
+      order.setQuantity(cart.getQuantity());
+      order.setOrderDate(LocalDateTime.now());
+      orderRepository.save(order);
+    }
+    // 注文後はカートを空にする
+    cartRepository.deleteAll();
   }
 }
   
